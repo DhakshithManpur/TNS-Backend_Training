a23ef0f093c808fedc53f466e80c21584e9a1841
Dhakshith Kumar <manpurdhakshithkumar21@gmail.com>
2025-05-12 15:46:20 +0530

    commit

---
+++ TNS/src/com/tns/variables/Student.java
@@ -0,0 +1,12 @@
+package com.tns.variables;
+
+public class Student {
+	public int sid;
+	String sname;
+	String CollegeName= "Sri Indu";
+	void display() {
+		System.out.println("The Details Of Student:"+sid+" "+sname+" "+CollegeName);
+	}
+}
+
+	
