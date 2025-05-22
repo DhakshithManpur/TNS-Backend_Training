ab76bf970b9e9e70ac1e1c5265d77adf0ab4f1a1
Dhakshith Kumar <manpurdhakshithkumar21@gmail.com>
2025-05-22 22:21:20 +0530

    commit

---
+++ TNS/src/com/tns/constructor/CopyConstructor.java
@@ -0,0 +1,14 @@
+package com.tns.constructor;
+
+public class CopyConstructor {
+	int a;
+	String name;
+	public Range(int a, String name){
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
